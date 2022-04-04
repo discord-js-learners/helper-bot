581dee2baecccc3c9afab2eb3e5b637575ac4712
realguystuff <101049350+realguystuff@users.noreply.github.com>
2022-04-04 10:32:07 +0800

    Add files via upload

--- handler.js
+++ handler.js
@@ -2,14 +2,14 @@ const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fet
 const ping = require('./commands/ping')
 const gif = require('./commands/gif')
 
-const command = { ping, gif }
+const commands = { ping, gif }
 
 module.exports = async function (message) {
     let tokens = message.content.split(" ");
     let command = tokens.shift();
     if (command.charAt(0) === '!') {
         command = command.substring(1);
-        commands[command](msg, tokens)
+        commands[command](message, tokens)
     } else {
         return;
     }
