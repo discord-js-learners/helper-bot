e9ec1cafa3fec61b28ede4dcdc59187d31517ec4
realguystuff <realguybackup@gmail.com>
2022-04-24 17:56:06 +0800

    updated randomarray calculator

--- commands/functions/randomarray.js
+++ commands/functions/randomarray.js
@@ -1,10 +1,4 @@
-module.exports = (array, object) => {
-	object = object || null;
-	let index = Math.floor(Math.random() * array.length);
-	if (object !== null) {
-		index = Math.floor(Math.random() * array.length.object);
-		return array[index].object;
-	} else {
-		return array[index];
-	}
+module.exports = (array) => {
+	const index = Math.floor(Math.random() * array.length);
+	return array[index];
 };
