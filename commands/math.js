8c412aaffae4bec2f14b3a84a58f4f96c1330341
realguystuff <realguybackup@gmail.com>
2022-05-02 19:06:30 +0800

    made math command efficient.

--- commands/math.js
+++ commands/math.js
@@ -1,5 +1,5 @@
-const name = 'add';
 const { MessageEmbed } = require('discord.js');
+const name = 'math';
 
 module.exports = (message, args) => {
 	if (args.length > 0) {
@@ -11,7 +11,11 @@ module.exports = (message, args) => {
 		} else if (args[1] === '*') {
 			result = args[0] * args[2];
 		} else if (args[1] === '/') {
-			result = args[0] / args[2];
+			if (args[2] === '0' || args[2] === 0) {
+				result = 'Can\'t divide by 0.';
+			} else {
+				result = args[0] / args[2];
+			}
 		} else {
 			result = 'NaN';
 		}
@@ -19,10 +23,11 @@ module.exports = (message, args) => {
 			console.log(`${message.member.user.tag} executed command '!${name}':`);
 			console.log(`${args}\n${args[0]} ${args[1]} ${args[2]} = ${result}`);
 			message.channel.send(`${result}`);
-		} else {
+		} else if (result === 'NaN' || result === 'Can\'t divide by 0.') {
 			const mathHelpEmbed = new MessageEmbed()
 				.setTitle('Math command help')
 				.setDescription('You\'ll need help for this hard command. **Remember to use space between numbers and operations**')
+				.setColor('RANDOM')
 				.addFields(
 					{ name: 'Addition:', value: '<num> + <num>' },
 					{ name: 'Subtraction:', value: '<num> - <num>' },
@@ -33,6 +38,17 @@ module.exports = (message, args) => {
 			message.channel.send({ embeds: [mathHelpEmbed] });
 		}
 	} else {
-		console.log(`${message.member.user.tag} executed command '!${name}', but didn't add the arguments.`);
+		console.log(`${message.member.user.tag} executed command '!${name}', but didn't add the arguments, so I gave help.`);
+		const mathHelpEmbed = new MessageEmbed()
+			.setTitle('Math command help')
+			.setDescription('You\'ll need help for this hard command. **Remember to use space between numbers and operations**')
+			.setColor('RANDOM')
+			.addFields(
+				{ name: 'Addition:', value: '<num> + <num>' },
+				{ name: 'Subtraction:', value: '<num> - <num>' },
+				{ name: 'Multiplication:', value: '<num> * <num>' },
+				{ name: 'Division:', value: '<num> / <num>' },
+			);
+		message.channel.send({ embeds: [mathHelpEmbed] });
 	}
 };
